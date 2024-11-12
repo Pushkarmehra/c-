d5a9bfe89a8ee433c9172f4d3334b3da88e3e494
Pushkar <pushkarjeemain@gmail.com>
2024-11-12 16:56:17 +0530

    Create data type modifier .cpp

---
+++ data type modifier .cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<< sizeof(int)<<endl;
+    cout<< sizeof( long int)<<endl;// add four bytes
+    cout<< sizeof( long long int)<<endl;// add eight bytes
+    cout<< sizeof( short int)<<endl;// only two bytes
+return 0;
+}
