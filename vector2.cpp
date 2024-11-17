c58895e00876a529b83782bd74cbf21fcb8ef6a1
Pushkar <pushkarjeemain@gmail.com>
2024-11-18 00:46:47 +0530

    Create vector2.cpp

---
+++ vector2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+//     vector<char> vec{'a','b','c','d','e','f','g','h','i','j','k','l','m'};
+//     cout<<vec[0]<<endl;
+//     // vector<int> vec2(3,0);
+//     // cout<<vec2[0]<<endl;
+//     // for(char i: vec){
+//     //     cout<<i<<endl;
+//     cout<<"size "<<vec.size()<<endl;
+//     vec.push_back('j');
+//     // for(char i: vec){
+//     //     cout<<i<<endl;
+//     // }
+//     vec.pop_back();
+//     // for(char i: vec){
+//     //     cout<<i<<endl;
+//     // }
+//     cout<<vec.front()<<endl;
+//     cout<<vec.at(9)<<endl;
+//     cout<<vec.back();
+vector<int> vv;
+vv.push_back(1);
+vv.push_back(2);
+vv.push_back(3);
+vv.push_back(4);
+vv.push_back(5);
+    cout<< vv.size()<<endl;
+    cout<<vv.capacity()<<endl;
+    return 0;
+}
